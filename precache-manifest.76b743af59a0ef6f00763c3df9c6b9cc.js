965b2d436e9fe2bb453e664fa5e1d21e6164eec0
Prenaam Dolou <prenaamd@gmail.com>
2020-04-03 12:37:57 -0500

    Updates

---
+++ precache-manifest.76b743af59a0ef6f00763c3df9c6b9cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6f4551a98e3002a521f44d7a5888bd3",
+    "url": "/pyd3690/SquareItAll.git/index.html"
+  },
+  {
+    "revision": "b266dd7e760776ada6f3",
+    "url": "/pyd3690/SquareItAll.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "60b5fdd9e79c04d5de76",
+    "url": "/pyd3690/SquareItAll.git/static/js/2.411d3b35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pyd3690/SquareItAll.git/static/js/2.411d3b35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b266dd7e760776ada6f3",
+    "url": "/pyd3690/SquareItAll.git/static/js/main.38e2f659.chunk.js"
+  },
+  {
+    "revision": "950d2908ea5c90a85a96",
+    "url": "/pyd3690/SquareItAll.git/static/js/runtime-main.980e8ada.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pyd3690/SquareItAll.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
